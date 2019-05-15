7905105daf65f874369578fbaa18cbe998587963
BHARATH CHENNAMKULAM RENJITH <44872201+cr-bharath@users.noreply.github.com>
2019-05-15 00:55:22 -0400

    Update data_generator.py

--- data_generator.py
+++ data_generator.py
@@ -4,6 +4,7 @@ import keras
 import glob
 import cv2
 
+TRAIN_DATA_APPEND = '/data/ILSVR2015/'
 class DataGenerator(keras.utils.Sequence):
     def __init__(self, path, list_id, folder_start_pos, batch_size=32, dim=(2, 240, 320), n_channels=3, shuffle=True):
         self.list_id = list_id
@@ -11,10 +12,10 @@ class DataGenerator(keras.utils.Sequence):
         self.batch_size = batch_size
         self.shuffle = shuffle
         self.path = path
-        self.train_data_path =self.path+'/data2/LITIV_dataset/Data/train/'
-        self.train_annot_path = self.path+'/data2/LITIV_dataset/Annotations/train/'
-        self.val_data_path = self.path+'/data2/LITIV_dataset/Data/val/'
-        self.val_annot_path = self.path+'/data2/LITIV_dataset/Annotations/val/'
+        self.train_data_path =self.path+TRAIN_DATA_APPEND + 'Data/train/'
+        self.train_annot_path = self.path+TRAIN_DATA_APPEND + 'Annotations/train/'
+        # self.val_data_path = self.path+'/data2/LITIV_dataset/Data/val/'
+        # self.val_annot_path = self.path+'/data2/LITIV_dataset/Annotations/val/'
         # self.label_list = self.get_labels()
         self.on_epoch_end()
         self.dim = dim
@@ -38,8 +39,6 @@ class DataGenerator(keras.utils.Sequence):
 
         folder = [dI for dI in os.listdir(self.train_data_path) if
                   os.path.isdir(os.path.join(self.train_data_path, dI))]
-        # print(folder)
-        # print(len(indexes))
         img_path_list = []
         for j, fileID in enumerate(indexes):
 
@@ -57,7 +56,6 @@ class DataGenerator(keras.utils.Sequence):
             else:
                 file_index = fileID - self.folder_start_pos[folder_index - 1]
 
-            # print("******")
             for train_len in range(2):
                 # After finding in which folder the file lives, do imread of that file
 
@@ -66,68 +64,12 @@ class DataGenerator(keras.utils.Sequence):
                 file_name = "{:04d}".format(file_index + 1 + train_len)
 
                 img_path = self.train_data_path + folder_name + "/" + file_name + ".jpg"
-                # print(img_path)
-                # img_path_list.append(img_path)
-                # print(len(img_path_list))
-                #     print("**********")
-                #     print(img_path)
-                # print(fileID,folder_index, file_index+train_len)
-                #     # #
                 img = cv2.imread(img_path)
                 X[j, train_len,] = img / 255.0
-                # bbox = self.get_labels(folder_name, file_index+train_len)
-                # print(bbox)
-                # cv2.rectangle(img,(bbox[0]-int(bbox[2]/2),bbox[1]-int(bbox[3]/2)), (bbox[0]+int(bbox[2]/2), bbox[1]+int(bbox[3]/2)), (0,255,0), 2)
-                # cv2.imshow('Chosen Image',img)
-                # cv2.waitKey(0)
             Y[j] = self.get_labels(folder_name, file_index)
-        # print(Y)
         return X, Y
 
-    # Validating dataset
-    def __data_generation_val(self, indexes):
-        folder = [dI for dI in os.listdir(self.val_data_path) if os.path.isdir(os.path.join(self.val_data_path, dI))]
-        # print(len(indexes))
-        img_path_list = []
-        for fileID in indexes:
-
-            # Find folder index and file index
-            folder_index = 0
-            file_index = 0
-
-            for k in range(len(self.folder_start_pos)):
-                if (fileID < (self.folder_start_pos[i])):
-                    folder_index = i
-                    break
-            if (folder_index == 0):
-                file_index = fileID
-            else:
-                file_index = fileID - self.folder_start_pos[folder_index - 1]
-
-            print("******")
-            for val_len in range(2):
-                # After finding in which folder the file lives, do imread of that file
-
-                folder_name = folder[folder_index]
-                file_name = "{:06d}".format(file_index + val_len)
-
-                img_path = self.val_data_path + folder_name + "/" + file_name + ".JPEG"
-
-                print(img_path)
-                print(folder_index, file_index + val_len)
-
-                img = cv2.imread(img_path)
-
-                # Resizing bounding box
-                # bbox = self.get_labels(folder_name, file_index+val_len)
-                # print(bbox)
-                # cv2.rectangle(img,(bbox[0]-int(bbox[2]/2),bbox[1]-int(bbox[3]/2)), (bbox[0]+int(bbox[2]/2), bbox[1]+int(bbox[3]/2)), (0,255,0), 2)
-                # cv2.imshow('Chosen Image', img)
-                # cv2.waitKey(0)
-
     def get_labels(self, folder_name, file_id):
-        #      print("Get labels is called")
-
         labels = []
         for i in sorted(glob.glob(self.train_annot_path+folder_name+"/*.txt")):
             labels = np.loadtxt(i, delimiter=',',dtype=int)
