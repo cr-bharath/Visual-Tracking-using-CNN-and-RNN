6a88fa0b58628ae7a6debbef3bb5620b870b7b6d
BHARATH CHENNAMKULAM RENJITH <44872201+cr-bharath@users.noreply.github.com>
2019-05-15 00:42:44 -0400

    Update network.py

--- network.py
+++ network.py
@@ -13,17 +13,20 @@ import keras
 
 
 def create_model(batch_size=None):
-  print("Batch size received is %d"%(batch_size))
+  """ Function to create a CNN-LSTM model for visual tracking"""
   K.set_learning_phase(1)
+  # You can replace iv3 with any other ImageNet pretrained models from Keras and compare performance of all
   cnn = iv3(
     weights='imagenet',
     include_top=False,
     pooling='max')
+  
   if batch_size != None:
     video = Input(shape=(2,240, 320, 3), name='video_input')
     encoded_frame = TimeDistributed(cnn)(video)
     encoded_vid = LSTM(1024,unroll=True)(encoded_frame)
   else:
+    # This is to create model while testing to set stateful=True
     video = Input(batch_shape=( 1,1,240, 320, 3),name='video_input')
     encoded_frame = TimeDistributed(cnn)(video)
     encoded_vid = LSTM(1024,unroll=False,stateful=True)(encoded_frame)
