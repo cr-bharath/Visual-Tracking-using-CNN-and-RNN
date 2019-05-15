f4ca239b8b16780571178ee6065f6ae171e400ac
BHARATH CHENNAMKULAM RENJITH <44872201+cr-bharath@users.noreply.github.com>
2019-05-15 00:46:45 -0400

    Update test.py

--- test.py
+++ test.py
@@ -4,13 +4,13 @@ import os
 import cv2
 import glob
 
-
+MAIN_DIR = os.getcwd()
+TESTING_DATA_FRAMES_PATH = MAIN_DIR + "/data/ILSVR2015/Data/test/*/*.JPEG"
 def main():
-    current_path = os.getcwd()
 
     test_model = create_model()
     test_model.load_weights('cnn_rnn_weights.hk5')
-    files = sorted(glob.glob(current_path+'/data2/LITIV_dataset/Data/val/*/*.jpg"))
+    files = sorted(glob.glob(TESTING_DATA_FRAMES_PATH))
     for i in range(1,len(files)):
       X = np.empty((1,1,240,320, 3))
       image = cv2.imread(files[i])
@@ -18,9 +18,8 @@ def main():
         X[0,j,] = cv2.imread(files[i-1+j])
       bbox = test_model.predict(X)
       bbox = np.int32(bbox[0])
-      cv2.rectangle(image,(bbox[0]-int(bbox[2]/2),bbox[1]-int(bbox[3]/2)), (bbox[0]+int(bbox[2]/2), bbox[1]+int(bbox[3]/2)), (0,255,0), 2)
-      #cv2.imshow('val',image)
-      cv2.imwrite(current_path+"/predictions/{0}.jpg".format(i),image)
+      cv2.rectangle(image,(bbox[0],bbox[1]), (bbox[2],bbox[3]), (0,255,0), 2)
+      cv2.imwrite(MAIN_DIR+"/predictions/{0}.jpg".format(i),image)
 
 
 if __name__ == '__main__':
